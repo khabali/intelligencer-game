b29d494f66ea4bdd8b65b4f1fad664291ea6e7f5
Anas KHABALI <mature.a@hotmail.fr>
2015-05-01 23:26:29 +0200

    the enemie waite alittle while before start moving

--- core/src/com/game/system/AIEnemiesMovementSystem.java
+++ core/src/com/game/system/AIEnemiesMovementSystem.java
@@ -5,20 +5,21 @@ import com.artemis.ComponentMapper;
 import com.artemis.Entity;
 import com.artemis.annotations.Mapper;
 import com.artemis.systems.EntityProcessingSystem;
+import com.badlogic.gdx.Gdx;
 import com.game.component.AIMovementComponent;
 import com.game.component.MovementComponent;
 import com.game.component.PositionComponent;
 
 public class AIEnemiesMovementSystem extends EntityProcessingSystem {
 
-	private final String tage = getClass().getName();
+	private final String tag = getClass().getName();
 
-	@Mapper
-	ComponentMapper<AIMovementComponent> _AIMovementCM;
-	@Mapper
-	ComponentMapper<PositionComponent> positionCM;
-	@Mapper
-	ComponentMapper<MovementComponent> movementCM;
+	@Mapper ComponentMapper<AIMovementComponent> _AIMovementCM;
+	@Mapper	ComponentMapper<PositionComponent> positionCM;
+	@Mapper	ComponentMapper<MovementComponent> movementCM;
+	
+	//
+	private float watingTime = 0.0f;
 
 	public AIEnemiesMovementSystem() {
 		super(Aspect.getAspectForAll(AIMovementComponent.class, PositionComponent.class, MovementComponent.class));
@@ -31,20 +32,24 @@ public class AIEnemiesMovementSystem extends EntityProcessingSystem {
 			AIMovementComponent _AImovement = _AIMovementCM.get(e);
 			PositionComponent position = positionCM.get(e);
 			MovementComponent movement = movementCM.get(e);
+			
 			if (!movement.isMoving) {
 				
-				//
-				int startCol = (int) position.defaultCol;
-				int startRow = (int) position.defaultRow;
-
-				//
-				int targetCol = movement.targetCol;
-				int targetRow = movement.targetRow;
-				movement.doMoveFromTo(startRow, startCol, targetRow, targetCol,
-						false);
+				watingTime += Gdx.graphics.getDeltaTime();
+				Gdx.app.debug(tag, "movingTime "+watingTime);
+				
+				if(watingTime >= _AImovement.waitingBeforRestart){
+					watingTime = 0.0f;
+					//
+					int startCol = (int) position.defaultCol;
+					int startRow = (int) position.defaultRow;
+	
+					//
+					int targetCol = movement.targetCol;
+					int targetRow = movement.targetRow;
+					movement.doMoveFromTo(startRow, startCol, targetRow, targetCol,	false);
+				}
 			}
-			
-			
 		}
 	}
 
